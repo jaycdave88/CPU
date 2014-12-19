711770ad40d6e3e8979ae166ca3d4aad224d3c33
Jay Dave <apple.com>
2014-12-19 10:12:14 -0800

    patch v4

--- RUNME.rb
+++ RUNME.rb
@@ -1,11 +1,11 @@
-require_relative "../CPU-master/Code/get_user_name.rb"
-require_relative "../CPU-master/Code/cpu1.rb"
+require_relative "./Code/get_user_name.rb"
+require_relative "./Code/cpu1.rb"
 
 puts "Please hit 'n' when you are done recording"
 user_input=gets.chomp
 if user_input == "n"
-	require_relative '../CPU-master/Code/get_user_name.rb'
-	require_relative '../CPU-master/Code/cpu2.rb'
+	require_relative './Code/get_user_name.rb'
+	require_relative './Code/cpu2.rb'
 	`open "/Users/#{$user_name_computer}/Desktop/CPU-master/Database/CPU_RESULTS.txt"`
 end
 
